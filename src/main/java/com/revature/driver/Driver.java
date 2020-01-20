e6b11ce362d345f842b24151fd15a9fc037fda23
Wes Otero <wesleyotero@protonmail.com>
2020-01-19 22:17:34 -0500

    Log Utiil Works

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -1,10 +1,11 @@
 package com.revature.driver;
 
+import com.revature.util.LogUtil;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Heyy!");
-
+		
 	}
 
 }
