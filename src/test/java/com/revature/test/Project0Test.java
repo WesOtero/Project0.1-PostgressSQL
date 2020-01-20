bfa39e9514303057c62dab33e10064c59540094c
Wes Otero <wesleyotero@protonmail.com>
2020-01-19 23:56:54 -0500

    Unit testing 0% success TDD initiated

--- src/test/java/com/revature/test/Project0Test.java
+++ src/test/java/com/revature/test/Project0Test.java
@@ -1,5 +1,86 @@
 package com.revature.test;
 
-public class Project0Test {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+
+public class Project0Test {
+	//As a user, I can login.
+	@Test
+	public void  testUserLogin() {
+		assertTrue(false);
+	}
+	
+	//As a user, I can register for a customer account.
+	@Test
+	public void  testUserRegistration() {
+		assertTrue(false);
+	}
+	
+	//As a customer, I can view the cars on the lot.
+	@Test
+	public void  testCustomerCarView() {
+		assertTrue(false);
+	}
+	
+	//As a customer, I can make an offer for a car.
+	@Test
+	public void  testCustomerOffer() {
+		assertTrue(false);
+	}
+	
+	//As a customer, I can view the cars that I own.
+	@Test
+	public void  testCustomerCarsOwnedView() {
+		assertTrue(false);
+	}
+	
+	//As a customer, I can view my remaining payments for a car.
+	@Test
+	public void  testRemainingPayments() {
+		assertTrue(false);
+	}
+	
+	//As an employee, I can remove a car from the lot.
+	@Test
+	public void  testRemoveCar() {
+		assertTrue(false);
+	}
+	
+	//As an employee, I can view all payments.
+	@Test
+	public void  testViewAllPayments() {
+		assertTrue(false);
+	}
+	
+	//As an employee, I can add a car to the lot.
+	@Test
+	public void  testAddCar() {
+		assertTrue(false);
+	}
+	
+	//As an employee, I can accept or reject an offer for a car.
+	@Test
+	public void  testAcceptOffer() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void  testRejectOffer() {
+		assertTrue(false);
+	}
+	
+	//As the system, I can calculate the monthly payment.
+	@Test
+	public void  testCalculatePayment() {
+		assertTrue(false);
+	}
+	
+	//As the system, I reject all other pending offers for a car when an offer is accepted.
+	@Test
+	public void  testRejectAllPending() {
+		assertTrue(false);
+	}
 }
+
---
+++ src/test/java/com/revature/test/Project0TestSuite.java
@@ -0,0 +1,10 @@
+package com.revature.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Project0Test.class})
+public class Project0TestSuite {
+	
+}
